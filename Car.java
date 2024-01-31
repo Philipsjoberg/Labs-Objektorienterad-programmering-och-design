5e4b2ec13f73a3bc685eaac1a6cd313e6b0b84a3
Philipsjoberg <118897693+Philipsjoberg@users.noreply.github.com>
2024-01-31 21:32:17 +0100

    Update Car.java

--- Car.java
+++ Car.java
@@ -4,18 +4,21 @@ public abstract class Car implements Movable {
     protected int nrDoors;
     protected double enginePower;
     protected double currentSpeed;
-    protected Color color;
-    protected String modelName;
+    private Color color;
+    public String modelName;
     private double xPos;
     private double yPos;
     private double xDir;
     private double yDir;
+    private Size size;
+    public enum Size { SMALL, MEDIUM, LARGE }
 
-    public Car(int nrDoors, double enginePower, Color color, String modelName) {
+    public Car(int nrDoors, double enginePower, Color color, String modelName, Size size) {
         this.nrDoors = nrDoors;
         this.enginePower = enginePower;
         this.color = color;
         this.modelName = modelName;
+        this.size = size;
         this.xPos = 0;
         this.yPos = 0;
         this.xDir = 0;
@@ -40,6 +43,11 @@ public abstract class Car implements Movable {
     protected double getxDir() { return xDir; }
     protected double getyDir() { return yDir; }
 
+    protected double setxPos(double amount) { return this.xPos = amount;}
+    protected double setyPos(double amount) { return this.yPos = amount;}
+    protected double setxDir(double amount) { return this.xDir = amount;}
+    protected double setyDir(double amount) { return this.yDir = amount;}
+
     protected Color getColor() {
         return color;
     }
@@ -47,6 +55,8 @@ public abstract class Car implements Movable {
     protected void setColor(Color clr) {
         color = clr;
     }
+
+    protected Size getsize() {return size; }
 
     protected void startEngine() {
         currentSpeed = 0.1;
@@ -57,10 +67,6 @@ public abstract class Car implements Movable {
     }
 
     protected abstract double speedFactor();
-
-    protected abstract void incrementSpeed(double amount);
-
-    protected abstract void decrementSpeed(double amount);
 
     public void gas(double amount) {
         if (amount < 0 || amount > 1) {
@@ -77,6 +83,7 @@ public abstract class Car implements Movable {
                 decrementSpeed(amount);
             }
     }
+
     public void move() {
         this.xPos += currentSpeed * this.xDir;
         this.yPos += currentSpeed * this.yDir;
@@ -126,6 +133,16 @@ public abstract class Car implements Movable {
             this.xDir = 0;
             this.yDir = -1;
         }
+    }
+    protected void incrementSpeed(double amount){
+        if (getCurrentSpeed() + speedFactor() * amount > currentSpeed)
+            currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+        if (currentSpeed > enginePower)
+            currentSpeed = enginePower;
+    }
+    protected void decrementSpeed(double amount){
+        if (getCurrentSpeed() - speedFactor() * amount < currentSpeed)
+            currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
     }
 }
 
