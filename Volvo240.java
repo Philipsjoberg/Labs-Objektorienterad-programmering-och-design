1f5d487bcb434a7a67c35cd689361fe4afe37bf2
Philipsjoberg <118897693+Philipsjoberg@users.noreply.github.com>
2024-01-31 21:34:32 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -5,26 +5,12 @@ public class Volvo240 extends Car {
     protected final static double trimFactor = 1.25;
 
     public Volvo240() {
-        super(4, 100, Color.black, "Volvo240");
+        super(4, 100, Color.black, "Volvo240", Size.MEDIUM);
     }
 
     @Override
     protected double speedFactor() {
         return enginePower * 0.01 * trimFactor;
-    }
-
-    @Override
-    protected void incrementSpeed(double amount) {
-        if (Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower) > currentSpeed) {
-            currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
-
-        }
-    }
-    @Override
-    protected void decrementSpeed(double amount) {
-        if (Math.max(getCurrentSpeed() - speedFactor() * amount, 0) < currentSpeed) {
-            currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
-        }
     }
 }
 
