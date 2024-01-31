11f72de9f882ab63e2531c53dee5dc94bc40254c
Philipsjoberg <118897693+Philipsjoberg@users.noreply.github.com>
2024-01-31 21:33:45 +0100

    Update Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -5,7 +5,7 @@ public class Saab95 extends Car {
     private boolean turboOn;
 
     public Saab95(){
-        super(2, 125, Color.red, "Saab95");
+        super(2, 125, Color.red, "Saab95", Size.SMALL);
         turboOn = false;
     }
 
@@ -22,20 +22,6 @@ public class Saab95 extends Car {
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
-    }
-
-    @Override
-    protected void incrementSpeed(double amount){
-        if (getCurrentSpeed() + speedFactor() * amount > currentSpeed)
-            currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-        if (currentSpeed > enginePower)
-            currentSpeed = enginePower;
-    }
-
-    @Override
-    protected void decrementSpeed(double amount){
-        if (getCurrentSpeed() - speedFactor() * amount < currentSpeed)
-            currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
     }
 }
 
